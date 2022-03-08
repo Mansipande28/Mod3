9cd0bdc4b7d4038715ee1cc9d97d88d0220ccc07
Mansipande28 <mansipande28@gmail.com>
2022-03-08 15:08:11 +0530

    first commit

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Demo-instance" {
-    ami = "ami-08b6f2a5c291246a0"
+    ami = "ami-0b9f27b05e1de14e9"
     instance_type = var.instance_type
     subnet_id = var.subnet1
     #security_group_id = var.security_group_id   
